48c321ef67ab3531726b6738cc93cf817e333486
kubotamari <kubotamari@172.19.10.176>
2017-05-01 08:36:01 +0900

    テストケースを作成しました。

--- snake-camel/src/snakecamel/SnakeCamelUtil.java
+++ snake-camel/src/snakecamel/SnakeCamelUtil.java
@@ -2,37 +2,37 @@ package snakecamel;
 
 public class SnakeCamelUtil {
 
-	public static String snakeToCamelcase(String snake_case) {
-		String[] words = snake_case.split("_");
+	public static String snakeToCamelcase(String snake_case) { // Snake→Camel
+		String[] words = snake_case.split("_"); // snake_caseで渡された文字列を"_"で分ける
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < words.length - 1; i++) {
-			sb.append(capitalize(words[i]));
+		for (int i = 0; i < words.length; i++) { // 分けた文字列の個数分 length-1→lengthに
+			sb.append(capitalize(words[i])); // 文字列の１文字目を大文字にしてappend
 		}
 		return new String(sb);
 	}
-	 
-	public static String camelToSnakecase(String camelcase) {
+	public static String camelToSnakecase(String camelcase) { // Camel → Snake
 		StringBuilder sb = new StringBuilder();
 		int j = 0;
-		for (int i = 0; i < camelcase.length(); i++) {
-			char c = camelcase.charAt(i);
-			if (Character.isUpperCase(c)) {
-				sb.append(camelcase.substring(j, i));
+		for (int i = 0; i < camelcase.length(); i++) { // 与えられた文字列の長さ
+			char c = camelcase.charAt(i); // 一文字ずつ読む
+			if (Character.isUpperCase(c)) { // 大文字だったら
+				String s = camelcase.substring(j, i);
+				if(s.length() > 0)
+					sb.append(uncapitalize(s)); // 文字列のj番目〜i番目をunacpitalizeして追加
 				if (sb.length() > 0) {
 					sb.append("_");
 				}
-				sb.append(Character.toLowerCase(c));
 				j = i;
 			}
 		}
-		sb.append(camelcase.substring(j));
+		sb.append(uncapitalize(camelcase.substring(j)));
 		return new String(sb);
 	}
 	
-	static String capitalize(String s) {
+	static String capitalize(String s) { // 文字列の最初の一字を大文字にする
 		char first = s.charAt(0);
 		char upperFirst = Character.toUpperCase(first);
-		String rest = s.substring(1);
+		String rest = s.substring(1); // 最初の一文字以外
 		return upperFirst + rest;
 	}
 
--- snake-camel/test/snakecamel/SnakeCamelTest.java
+++ snake-camel/test/snakecamel/SnakeCamelTest.java
@@ -1,14 +1,52 @@
 package snakecamel;
 
 import static org.junit.Assert.*;
-
+import static org.hamcrest.CoreMatchers.*;
 import org.junit.Test;
 
 public class SnakeCamelTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void snakeToCamelcaseでengをCamelcaseにして取得する() {
+	SnakeCamelUtil scu = new SnakeCamelUtil();
+	String expected = "Snake";
+	String actual = scu.snakeToCamelcase("snake");
+	assertThat(actual, is(expected));
 	}
-
+	@Test
+	public void snakeToCamelcaseでeng_aveをCamelcaseにして取得する() {
+	SnakeCamelUtil scu = new SnakeCamelUtil();
+	String expected = "SnakeCamel";
+	String actual = scu.snakeToCamelcase("snake_camel");
+	assertThat(actual, is(expected));
+	}@Test
+	public void snakeToCamelcaseでEngAveTestをCamelcaseにして取得する() {
+	SnakeCamelUtil scu = new SnakeCamelUtil();
+	String expected = "SnakeCamelTest";
+	String actual = scu.snakeToCamelcase("snake_camel_test");
+	assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void camelToSnakecaseでengをSnakecaseにして取得する() {
+	SnakeCamelUtil scu = new SnakeCamelUtil();
+	String expected = "sna";
+	String actual = scu.camelToSnakecase("Sna");
+	assertThat(actual, is(expected));
+	}
+	@Test
+	public void camelToSnakecaseでEngAveをSnakecaseにして取得する() {
+	SnakeCamelUtil scu = new SnakeCamelUtil();
+	String expected = "snake_camel";
+	String actual = scu.camelToSnakecase("SnakeCamel");
+	assertThat(actual, is(expected));
+	}
+	@Test
+	public void camelToSnakecaseでEngAveTestをSnakecaseにして取得する() {
+	SnakeCamelUtil scu = new SnakeCamelUtil();
+	String expected = "snake_camel_test";
+	String actual = scu.camelToSnakecase("SnakeCamelTest");
+	assertThat(actual, is(expected));
+	}
+	
 }
